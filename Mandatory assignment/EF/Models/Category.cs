1ce802843de5d3686206e0ba150712882b75cc9e
doncusspelunkus <marcuspetersen@live.dk>
2024-02-28 21:18:33 +0100

    Revert "Fix Folder structure"
    This reverts commit 76e71c0209f7a9b5d225e02703702686d5a23ed7.
    
    Reverting changes to main

--- Mandatory assignment/EF/Models/Category.cs
+++
@@ -1,7 +0,0 @@
-namespace EF_Compulsory_Assignment.Model;
-
-public class Category
-{
-    public int CategoryId { get; set; }
-    public string CategoryName { get; set; }
-}
--- Mandatory assignment/SQL-Scripts/Compulsory-Assignment/EF/Models/Product.cs
+++ Mandatory assignment/SQL-Scripts/Compulsory-Assignment/EF/Models/Product.cs
@@ -5,8 +5,6 @@ public class Product
     public string Name { get; set; }
     public decimal Price { get; set; }
     public string Description { get; set; }
-
-    public int CategoryId { get; set; }
     
 }
 
